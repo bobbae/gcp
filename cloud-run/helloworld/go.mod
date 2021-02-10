0583cfd373b96e6c8ede820abd101ff5c3af836b
bob bae <bob.bae@gmail.com>
2021-02-10 14:54:21 -0800

    xkcd

--- cloud-run/helloworld/go.mod
+++ cloud-run/helloworld/go.mod
@@ -1,3 +1,5 @@
 module github.com/bobbae/gcp/cloud-run/helloworld
 
 go 1.13
+
+require github.com/nishanths/go-xkcd v2.0.1-0.20200515110959-4726b81e9134+incompatible
--- cloud-run/helloworld/main.go
+++ cloud-run/helloworld/main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
+	"context"
+	"encoding/json"
+	"html/template"
+
+	"github.com/nishanths/go-xkcd"
 )
+
+type Xkcd struct {
+	Title string `json:"title"`
+	URL string `json:"url"`
+}
 
 func main() {
 	log.Print("starting server...")
@@ -18,6 +30,8 @@ func main() {
 		log.Printf("defaulting to port %s", port)
 	}
 
+	http.HandleFunc("/xkcd", xkcdHandler)
+	http.HandleFunc("/json", jsonHandler)
 	// Start HTTP server.
 	log.Printf("listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
@@ -31,4 +45,43 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		name = "World"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
+}
+
+func getXkcd() (Xkcd) {
+	client := xkcd.NewClient()
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	comic, err := client.Get(context.Background(), r1.Intn(2421)+1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	xkcd := Xkcd {
+		Title: comic.Title,
+		URL: comic.ImageURL,
+	}
+	return xkcd
+}
+
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","application/json")
+	
+	json.NewEncoder(w).Encode(getXkcd())
+}
+
+func xkcdHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t, err:= template.New("foo").Parse(`{{define "FOO"}}
+		<html><head></head><center>
+		<body><div id="comic"><img src="{{.URL}}" alt="{{.Title}}" />
+		<br><center>{{.Title}}</center></div></body>
+		</center>
+		</html>
+		{{end}}
+		`,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.ExecuteTemplate(w, "FOO", getXkcd())
 }

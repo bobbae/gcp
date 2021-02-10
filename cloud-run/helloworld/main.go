b541bbb2b28b5f9e1537a66357c96918af9dfeeb
bob bae <bob.bae@gmail.com>
2021-02-10 15:50:39 -0800

    css

--- cloud-run/helloworld/main.go
+++ cloud-run/helloworld/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"time"
-	"context"
-	"encoding/json"
-	"html/template"
 
 	"github.com/nishanths/go-xkcd"
 )
 
 type Xkcd struct {
 	Title string `json:"title"`
-	URL string `json:"url"`
+	URL   string `json:"url"`
 }
 
 func main() {
@@ -47,7 +47,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!\n", name)
 }
 
-func getXkcd() (Xkcd) {
+func getXkcd() Xkcd {
 	client := xkcd.NewClient()
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
@@ -55,29 +55,40 @@ func getXkcd() (Xkcd) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	xkcd := Xkcd {
+	xkcd := Xkcd{
 		Title: comic.Title,
-		URL: comic.ImageURL,
+		URL:   comic.ImageURL,
 	}
 	return xkcd
 }
 
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
-	
+	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(getXkcd())
 }
 
 func xkcdHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	t, err:= template.New("foo").Parse(`{{define "FOO"}}
-		<html><head></head><center>
-		<body><div id="comic"><img src="{{.URL}}" alt="{{.Title}}" />
-		<br><center>{{.Title}}</center></div></body>
-		</center>
-		</html>
-		{{end}}
-		`,
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t, err := template.New("foo").Parse(`{{define "FOO"}}
+<html><head>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&display=swap" rel="stylesheet">
+<style>body { font-family: 'Patrick Hand SC', cursive; font-size: 48px; }
+</style></head><body class="text-center">
+<div id="container" class="jumbotron">
+	<div class="d-grid gap-3">
+		<div class="p-2 bg-light border">
+			<img src="{{.URL}}" alt="{{.Title}}" />
+		</div>
+		<div class="p-2 bg-light border">
+			{{.Title}}
+		</div>
+	</div>
+</div>
+</body></html>{{end}}
+`,
 	)
 
 	if err != nil {

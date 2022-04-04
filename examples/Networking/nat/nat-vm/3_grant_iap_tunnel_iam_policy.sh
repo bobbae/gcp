e02b7c92e8116c1863e1c07126b9d283d7f468fc
bob bae <bob.bae@gmail.com>
2022-04-04 19:01:16 +0000

    defang

--- examples/Networking/nat/nat-vm/3_grant_iap_tunnel_iam_policy.sh
+++ examples/Networking/nat/nat-vm/3_grant_iap_tunnel_iam_policy.sh
@@ -1,5 +1,5 @@
 # PROJECT_ID
-# MEMBER_INFO     user:bob.bae@sada.com    group:admins@example.com    serviceAccount:test123@example.domain.com
-gcloud projects add-iam-policy-binding acto-su-1 \
-    --member=user:bob.bae@sada.com \
+# MEMBER_INFO     user:username@example.com    group:admins@example.com    serviceAccount:test123@example.domain.com
+gcloud projects add-iam-policy-binding my-project-1 \
+    --member=user:bob@example.com \
     --role=roles/iap.tunnelResourceAccessor

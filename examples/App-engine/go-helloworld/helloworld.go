5a41f033222dff3bff8308399c6114e57b6926e7
bob bae <bob.bae@gmail.com>
2022-07-05 16:42:36 -0700

    app engine golang example

---
+++ examples/App-engine/go-helloworld/helloworld.go
@@ -0,0 +1,64 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START gae_go111_app]
+
+// Sample helloworld is an App Engine app.
+package main
+
+// [START import]
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// [END import]
+// [START main_func]
+
+func main() {
+	fileServer := http.FileServer(http.Dir("."))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
+}
+
+// [END main_func]
+
+// [START indexHandler]
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
+
+// [END indexHandler]
+// [END gae_go111_app]
